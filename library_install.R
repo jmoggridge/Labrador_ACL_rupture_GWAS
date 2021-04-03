44a16b87b40406072a8ce90657e27c8db4dcec76
jmoggridge <ja.moggridge@gmail.com>
2021-04-03 17:04:44 -0400

    need to write a script to remove one sample from each related pair with pi_hat > 0.2 (or chosen threshold)

---
+++ library_install.R
@@ -0,0 +1,4 @@
+install.packages('tidyverse', repos='https://cloud.r-project.org/')
+install.packages('rcartocolor', repos='https://cloud.r-project.org/')
+install.packages('glue', repos='https://cloud.r-project.org/')
+install.packages('tidymodels', repos='https://cloud.r-project.org/')
---
+++ split_related_pairs.R
@@ -0,0 +1,15 @@
+# tutorial needs a script to select out related pairs. (not by hand)
+# generally, want to drop the member with more missing data.
+# could try to find the individuals most related to others?
+
+
+# df <- read.table("plink.imiss", header = T)
+# snps <- read.table("min0.2_something from last part of QC)
+# pick <- function(row_input){
+#   
+# }
+# 
+# to_drop <- list()[1:nrow(df)]
+# for i in seq_along(nrow(df)){
+#   
+# }
